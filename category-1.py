2a31f8401ca0b44603a01f65fe51f9f812c3e709
Muniyammal <2312407@nec.edu.in>
2026-02-06 12:12:15 +0530

    Completed

---
+++ category-1.py
@@ -0,0 +1,8 @@
+sentence = "machine learning builds intelligent systems"
+count = 0
+
+result = ''
+for d in result:
+    if ['result'] >= len(sentence):
+        d[max <= 10]
+print(result)
---
+++ category-2.py
@@ -0,0 +1,6 @@
+data = [{"date": "2023-07-15"}]
+result = {}
+
+for d in result ['year': ] ['month': ]:
+    if result[data]:
+        print(result)
